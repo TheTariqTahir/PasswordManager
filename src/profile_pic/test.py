08d106df61b6ebd2c16f5ae00a2d5ab5ef719c63
Tariq-Dev <68277999+TheTariqTahir@users.noreply.github.com>
2022-07-05 17:27:24 +0500

    Add files via upload

---
+++ src/profile_pic/test.py
@@ -0,0 +1,2 @@
+x={'Category': 'facebook', 'Email': 'abc@example.com', 'Hint': 'example hint', 'Password': 'examplepassword', 'fav_icon': 'heart-outline', 'key': '2022-05-16 19:15:29.660340'}
+print(x['Category'])

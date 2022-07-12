8041d4ee7d3e192f652916fb1e34d440abf09e5f
Tariq-Dev <68277999+TheTariqTahir@users.noreply.github.com>
2022-07-12 17:12:52 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-import sys
-from PyQt5 import QtWidgets,QtGui,QtCore
-from PyQt5.QtWebEngineWidgets import *
-app=QtWidgets.QApplication(sys.argv)
-w=QWebEngineView()
-w.load(QtCore.QUrl('https://google.com')) ## load google on startup
-w.showMaximized()
-app.exec_()
+# import random
+# tags = ['#instagood', '#meme',"#love","#photooftheday","#fashion","#beautiful","#happy","#picoftheday","#memesdaily","#likeforlikes","#followme","#memesdaily", '#funny', '#funnymemes', '#humor', '#dank', '#comedy', '#instagram', '#dankmeme', '#tiktok','#lmao', '#dailymemes', '#funnymeme', '#explorepage']
+
+# random.shuffle(tags)
+
+# new_tags = ' '.join(tags[:10])
+
+# result_Tag = f":D :D\nFollow @mr_meemer_69\n.\n.\n.\n#pakistanimemes  #memes #dankmemes #memepage #edgymemes #offensivememes #shitpost #funny #follow4followback #like4likes {new_tags}"
+# print(result_Tag)
+
+
+import kivymd
+print(kivymd.__version__)
